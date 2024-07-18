9c4cc8a3605b47d695ebdd56f0617c1d433eea95
Serkan Yalçın <serkan.syalcin@hotmail.com>
2024-07-18 23:53:01 +0530

    Migration created

---
+++ database/migrations/2024_07_18_181919_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('provider_name')->nullable();
+            $table->string('name')->nullanle();
+            $table->time('duration')->nullable();
+            $table->integer('difficulty_level')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
